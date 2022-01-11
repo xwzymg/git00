87b37e330461ea1efee34f4f817b6f6c649a9682
xwzymg <2409942244@qq.com>
2022-01-11 22:30:51 +0800

    push commit

--- src/main/java/com/henu/Git.java
+++ src/main/java/com/henu/Git.java
@@ -4,6 +4,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("heloo git!");
         System.out.println("heloo git2!");
+        System.out.println("push test");
 
     }
 }
